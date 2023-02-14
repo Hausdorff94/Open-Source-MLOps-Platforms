c87eb15ced8680f66439fa87a447b1ad6b6a70b9
Johny Vallejo-Sánchez <35435216+Hausdorff94@users.noreply.github.com>
2023-02-14 05:01:07 +0000

    Update train script

--- train.py
+++ train.py
@@ -15,10 +15,47 @@ def eval_metrics(actual, pred):
     rmse = np.sqrt(mean_squared_error(actual, pred))
     mae = mean_absolute_error(actual, pred)
     r2 = r2_score(actual, pred)
+
     return rmse, mae, r2
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
     np.random.seed(936)
 
-    wine_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+    # Read data
+    data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "wine-quality.csv")
+    df = pd.read_csv(data_path)
+
+    # Split data into train and test
+    train, test = train_test_split(df)
+
+    # Select features and target
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=936)
+        lr.fit(train_x, train_y)
+
+        predictions = lr.predict(test_x)
+
+        (rmse, mae, r2) = eval_metrics(test_y, predictions)
+
+        print(f"Elasticnet model (alpha={alpha}, l1_ratio={l1_ratio}):")
+        print(f"\tRMSE: {rmse}")
+        print(f"\tMAE: {mae}")
+        print(f"\tR2: {r2}")
+
+        mlflow.log_param("alpha", int(alpha))
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("r2", r2)
+        mlflow.log_metric("mae", mae)
+
+        mlflow.sklearn.log_model(lr, "model")
+
